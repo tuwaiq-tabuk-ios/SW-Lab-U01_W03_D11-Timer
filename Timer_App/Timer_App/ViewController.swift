16bad5142eca04a3ba50e7c75b17a418fb14a06c
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-10-18 11:06:55 +0300

    finsh home work

--- Timer_App/Timer_App/ViewController.swift
+++ Timer_App/Timer_App/ViewController.swift
@@ -10,70 +10,75 @@ import AVFoundation
 
 class ViewController: UIViewController {
   
-  @IBOutlet weak var textTimer: UILabel!
-  @IBOutlet weak var timer: UIProgressView!
+  
+  @IBOutlet weak var questionLabel: UILabel!
+  
+  @IBOutlet weak var progressView: UIProgressView!
+  
+  @IBOutlet weak var percentageLabel: UILabel!
+  
   
   var player: AVAudioPlayer?
-  var progressTimer:Timer!
   
-  var denteTime = 420.0
-  var normalTime = 660.0
+  
+  let denteTime = 420.0
+  let normalTime = 660.0
+  var secondsLeft = 0.0
+  var totelTimer = 0.0
+  var secondsPassed = 0.0
+  var timer : Timer!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    timer.progress = 0.0
+    progressView.progress = 0.0
     
     // Do any additional setup after loading the view.
   }
   
-  @IBAction func lleftButton(_ sender: UIButton) {
-    textTimer.text = "Boiling pasta al dente"
-    denteTime = 420.0
+  @IBAction func boilingTypePressed(_ sender: UIButton) {
+    let bolingType = sender.currentTitle!
     stopSound()
-    self.progressTimer = Timer.scheduledTimer(timeInterval: 1.0,target: self,selector:#selector(updateCounter),userInfo: "Dente",repeats: true)
+    if bolingType == "Normal"{
+      secondsLeft = normalTime
+      secondsPassed = 0
+      totelTimer = normalTime
+      progressView.progress = 0.0
+      percentageLabel.text = "0%"
+      questionLabel.text = "Boiling pasta  normal time"
+    }else{
+      secondsLeft = denteTime
+      secondsPassed = 0
+      totelTimer = denteTime
+      percentageLabel.text = "0%"
+      progressView.progress = 0.0
+      questionLabel.text = "Boiling pasta  dente time"
+    }
+    timer = Timer.scheduledTimer(timeInterval: 1.0,target: self,selector:#selector(updateCounter),userInfo: "Dente",repeats: true)
   }
   
-  @IBAction func rightButton(_ sender: UIButton) {
-    textTimer.text = "Boiling pasta al normal"
-    normalTime = 660.0
-    stopSound()
-    self.progressTimer = Timer.scheduledTimer(timeInterval: 1.0,target: self,selector:#selector(updateCounter),userInfo: "Normal",repeats: true)
-  }
   
   
   @objc func updateCounter() {
     //example functionality
-    timer.progress = 0.0
-    let name = progressTimer.userInfo as! String
-    if name == "Dente" {
-      if denteTime > 0 {
-        timer.setProgress(Float(denteTime), animated: true)
-        
-        print("\(denteTime) seconds to the end of the world")
-        denteTime -= 1
-      }else{
-        textTimer.text = "TIME IS OVER! \n Remove pot from heat"
-        progressTimer.invalidate()
-        playSound()
-        
-      }
+    secondsPassed += 1
+    
+    
+    if secondsLeft > 0 {
+      let percentageProgress = secondsPassed / totelTimer
+      percentageLabel.text = "\(Int(percentageProgress * 100))%"
+      progressView.progress = Float(percentageProgress)
+      print("\(secondsLeft) seconds to the end of the world")
+      secondsLeft -= 1
     }else{
-      if normalTime > 0 {
-        timer.setProgress(Float(normalTime), animated: true)
-        
-        print("\(normalTime) seconds to the end of the world")
-        normalTime -= 1
-      }else{
-        textTimer.text = "TIME IS OVER! \n Remove pot from heat"
-        progressTimer.invalidate()
-        playSound()
-        
-      }
+      timer.invalidate()
+      questionLabel.text = "TIME IS OVER! \n Remove pot from heat"
+      playSound()
+      
     }
     
+    
   }
-  
-  
   
   func playSound() {
     guard let url = Bundle.main.url(forResource: "alarm", withExtension: "mp3") else { return }
@@ -95,10 +100,19 @@ class ViewController: UIViewController {
     }
   }
   
-  func stopSound() {
+  func stopSound () {
     player?.stop()
   }
   
   
   
 }
+
+
+
+
+
+
+
+
+
